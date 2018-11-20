89c9b7d51f645b674779e4b5811097648302c434
cla15121475 <cla15121475@CLCLRCAC20.belfastmet.int>
2018-11-20 15:59:05 +0000

    toy story example

--- 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbController/controller/ToyStory.java
+++ 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbController/controller/ToyStory.java
@@ -1,5 +1,24 @@
 package uk.ac.belfastmet.sbController.controller;
 
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
 public class ToyStory {
+	
+	@GetMapping("/woody")
+	public String woody (Model model) {
+		return "Woody was played by Tom Hanks";
+	}
+	
+	@GetMapping("/buzz")
+	public String buzz (Model model) {
+		return "Buzz was played by Tim Allen";
+	}
 
 }
