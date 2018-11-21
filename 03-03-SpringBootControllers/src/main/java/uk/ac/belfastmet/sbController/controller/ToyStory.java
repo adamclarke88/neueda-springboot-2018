73c9f1b62ba3a1ce9d77313d8b1224941c1b74fd
cla15121475 <cla15121475@CLCLRCAC20.belfastmet.int>
2018-11-21 15:47:44 +0000

    Created a website showing dwarfs through springboot

--- 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbController/controller/ToyStory.java
+++ 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbController/controller/ToyStory.java
@@ -5,9 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestController
 @RequestMapping("/toystory")
 
 public class ToyStory {
