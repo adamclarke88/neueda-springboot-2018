7a35d64a3e0b7750509869deea001de3052c6bbc
adamclarke888 <adamclarke88@gmail.com>
2018-12-10 13:58:50 +0000

    Created new rest api project

---
+++ 06-01-TitanicRestAPI/src/main/java/uk/ac/belfastmet/titanicrestapi/repository/PassengerRepository.java
@@ -0,0 +1,12 @@
+package uk.ac.belfastmet.titanicrestapi.repository;
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.belfastmet.titanicrestapi.domain.Passenger;
+
+
+public interface PassengerRepository extends CrudRepository<Passenger, Integer> {
+
+	Passenger findByPassengerId(Integer passengerId);
+
+
+
+}
